2318311af92be0302d0a3a76a1aa2682a8e648bc
Igor <igor.krupcko2010@yandex.ru>
2023-06-10 03:44:22 +0300

    Added filter cities and endpoint

---
+++ backend/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from .views import CityViewSet
+
+
+app_name = 'api'
+
+router = DefaultRouter()
+
+router.register('cities', CityViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
