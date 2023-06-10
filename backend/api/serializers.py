33ef8fbcddb17ca182c6fb1646d6d2b1cd40189e
Veresk <ZLveresk@gmail.com>
2023-06-10 14:35:28 +0300

    add ticket

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,3 +7,20 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = '__all__'
+
+class TicketSerializer(serializers.Serializer):
+
+    origin = serializers.CharField(max_length=10)
+    destination = serializers.CharField(max_length=10)
+    origin_airport = serializers.CharField(max_length=10)
+    destination_airport = serializers.CharField(max_length=10)
+    price = serializers.IntegerField()
+    airline = serializers.CharField(max_length=10)
+    flight_number = serializers.CharField(max_length=10)
+    departure_at = serializers.DateField()
+    return_at = serializers.DateField()
+    transfers = serializers.IntegerField()
+    return_transfers = serializers.IntegerField()
+    duration = serializers.IntegerField()
+    link = serializers.URLField()
+    currency = serializers.CharField(max_length=10)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,14 @@
+import requests
+
 from rest_framework import filters, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from tickets.models import City
-from .serializers import CitySerializer
+from .serializers import CitySerializer, TicketSerializer
+
+
+URL_2 = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
 
 
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
@@ -9,3 +16,20 @@ class CityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = City.objects.all()
     filter_backends = (filters.SearchFilter,)
     search_fields = ('^name',)
+
+
+HEADERS = {
+    'origin': 'MOW',
+    'destination': 'DXB',
+    'departure_at': '2023-07',
+    'return_at': '2023-08',
+    'market': 'ru',
+    'token': 'fc0490ceb2e8db1f3f383adb035ea3e2'
+}
+
+
+class TickerAirline(APIView):
+    def get(self, request, format=None):
+        response_data = requests.get(URL_2, params=HEADERS,).json()
+        my_serializer = TicketSerializer(data=response_data, many=True)
+        return Response(data=my_serializer.initial_data)
