30aff56bbe5d186309c5c8f3d549de4a663ffe80
Veresk <ZLveresk@gmail.com>
2023-06-12 14:52:45 +0300

    Add filter_time

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,9 +6,8 @@ from rest_framework.decorators import api_view
 
 from tickets.models import City
 from .serializers import CitySerializer, TicketSerializer
-
-
-URL_2 = 'https://api.travelpayouts.com/aviasales/v3/prices_for_dates'
+from .filter import sort_by_time
+from .constants import URL_SEARCH, COUNT_TICKET
 
 
 class CityViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,12 +18,20 @@ class CityViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 @api_view(['POST'])
-def get_ticket(request, format=None):
+def find_ticket(request, format=None):
     """Функция для поиска билетов."""
 
-    if request.method == 'POST':
-        HEADERS = request.data
-        HEADERS['token'] = 'fc0490ceb2e8db1f3f383adb035ea3e2'
-        response_data = requests.get(URL_2, params=HEADERS,).json()
+    HEADERS = request.data
+    HEADERS['token'] = 'fc0490ceb2e8db1f3f383adb035ea3e2'
+    print(HEADERS)
+    if HEADERS['sorting'] == 'time':
+        HEADERS['sorting'] = 'price'
+        HEADERS['limit'] = COUNT_TICKET
+        response_data = requests.get(URL_SEARCH, params=HEADERS,).json()
+        response_data = sort_by_time(response_data)
+        my_serializer = TicketSerializer(data=response_data, many=True)
+        return Response(data=my_serializer.initial_data)
+    else:
+        response_data = requests.get(URL_SEARCH, params=HEADERS,).json()
         my_serializer = TicketSerializer(data=response_data, many=True)
         return Response(data=my_serializer.initial_data)
