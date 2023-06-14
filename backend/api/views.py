79df8bbf3ce545d9398346c03338625324eb0927
Алексей Горячев <A1.Goryachev@yandex.ru>
2023-06-14 18:31:58 +0300

    isorted

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import filters, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from tickets.models import City
+
 from .serializers import CitySerializer
 from .utils import get_calendar_days
 
