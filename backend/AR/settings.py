b998b30c182e8a2c003987e89a9c2ed2dafdf0c5
Алексей Николаев <dkphobos@yandex.ru>
2023-06-19 20:49:07 +0300

    fix settings bd

--- backend/AR/settings.py
+++ backend/AR/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'AR.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
 
