86e5f2548c79338db1e9dc2b330e03e933ef83fe
Алексей Горячев <A1.Goryachev@yandex.ru>
2023-07-03 12:18:39 +0300

    created models and admin

--- backend/categories/admin.py
+++ backend/categories/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Activity, Travel, TravelActivity
+
+
+EMPTY_DISPLAY = '-пусто-'
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'start_date', 'start_time',
+                    'address', 'phone', 'website', 'email', 'price')
+    search_fields = ('name', 'start_date')
+    list_filter = ('name', 'start_date')
+    empty_value_display = EMPTY_DISPLAY
+
+
+@admin.register(Travel)
+class TravelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'activity')
+    search_fields = ('name', 'author')
+    list_filter = ('name', 'author')
+    empty_value_display = EMPTY_DISPLAY
+
+
+@admin.register(TravelActivity)
+class TravelActivityAdmin(admin.ModelAdmin):
+    list_display = ('travel', 'activity')
+    search_fields = ('travel', 'author')
+    list_filter = ('travel', 'author')
+    empty_value_display = EMPTY_DISPLAY
--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,2 +1,90 @@
 from django.db import models
+from users.models import User
 
+
+class Activity(models.Model):
+    """Модель активностей."""
+
+    name = models.CharField(verbose_name='Название события',
+                            help_text='Укажите название',
+                            max_length=50)
+    description = models.TextField(verbose_name='Описание',
+                                   help_text='Укажите описание')
+    start_date = models.DateField(verbose_name='Дата начала',
+                                  help_text='Укажите дату начала')
+    start_time = models.TimeField(verbose_name='Время начала',
+                                  help_text='Укажите время начала')
+    address = models.CharField(verbose_name='Адрес',
+                               help_text='Укажите адрес')
+    phone = models.CharField(verbose_name='Телефон',
+                             help_text='Укажите телефон',
+                             max_length=20)
+    website = models.CharField(verbose_name='Сайт',
+                               help_text='Укажите сайт')
+    email = models.EmailField(verbose_name='Электронная почта',
+                              help_text='Укажите почту')
+    price = models.CharField(verbose_name='Цена',
+                             help_text='Укажите цену')
+
+    class Meta:
+        """Meta модели Activity."""
+
+        ordering = ['-start_date', '-start_time']
+        verbose_name = 'Активность'
+        verbose_name_plural = 'Активности'
+
+    def __str__(self) -> str:
+        """Функция __str__ модели Activity."""
+        return self.name
+
+
+class Travel(models.Model):
+    """Модель путешествий."""
+
+    name = models.CharField(verbose_name='Имя путешествия',
+                            help_text='Назовите свое путешествие',
+                            max_length=50)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='travel',
+        verbose_name='Автор',
+        help_text='Укажите автора'
+    )
+    activity = models.ManyToManyField()
+
+    class Meta:
+        """Meta модели Travel."""
+
+        verbose_name = 'Путешествие'
+        verbose_name_plural = 'Путешествия'
+
+    def __str__(self) -> str:
+        """Функция __str__ модели Activity."""
+        return self.name
+
+
+class TravelActivity(models.Model):
+    """Модель для связи моделей Travel Activity."""
+
+    travel = models.ForeignKey(Travel,
+                               on_delete=models.CASCADE,
+                               related_name='travel')
+    activity = models.ForeignKey(Activity,
+                                 on_delete=models.CASCADE,
+                                 related_name='activity')
+
+    class Meta:
+        """Meta модели TravelActivity."""
+
+        verbose_name = 'Активность в путешествии'
+        verbose_name_plural = 'Активности в путешествии'
+        constraints = [models.UniqueConstraint(
+            fields=['travel', 'activity'],
+            name='unique_activity'
+        )]
+
+    def __str__(self) -> str:
+        """Функция __str__ модели TravelActivity."""
+        return (f'Активность {self.activity.name}'
+                f'в путешествии {self.travel.name}')
