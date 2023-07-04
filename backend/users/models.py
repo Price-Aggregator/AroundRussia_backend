10d71bb84ba4f11e900061c3bca45feb1b587ac1
Покровский Сергей <sergee13@yandex.ru>
2023-07-04 13:09:55 +0300

    Fix users models and CICD for frontend

--- backend/users/models.py
+++ backend/users/models.py
@@ -10,6 +10,7 @@ SEX_CHOICES = (
 class User(AbstractUser):
     email = models.EmailField('Электронная почта', unique=True)
     sex = models.CharField('Пол', max_length=1, choices=SEX_CHOICES,
-                           blank=True)
-    phone_number = models.IntegerField('Телефонный номер', blank=True)
-    birth_date = models.DateField('Дата рождения', blank=True)
+                           blank=True, null=True)
+    phone_number = models.IntegerField('Телефонный номер', blank=True,
+                                       null=True)
+    birth_date = models.DateField('Дата рождения', blank=True, null=True)
