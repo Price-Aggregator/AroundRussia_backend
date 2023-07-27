703ef868f448f8ca14a2e5888fca784cc582affb
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-07-27 18:11:41 +0300

    Added exception to fill_database and dates validation to create method of TravelSerializer

--- backend/tickets/management/commands/fill_database_cities.py
+++ backend/tickets/management/commands/fill_database_cities.py
@@ -9,12 +9,18 @@ class Command(BaseCommand):
     help = 'Filling the database with cities'
 
     def handle(self, *args, **kwargs) -> None:
-        City.objects.bulk_create(
-            City(
-                code=city['code'],
-                name=city['name'],
-                latitude=city['coordinates']['lat'],
-                longitude=city['coordinates']['lon'],
-            ) for city in requests.get(URL_WITH_CITIES).json()
-            if city['country_code'] == 'RU' and city.get('name') is not None
-        )
+        try:
+            City.objects.bulk_create(
+                City(
+                    code=city['code'],
+                    name=city['name'],
+                    latitude=city['coordinates']['lat'],
+                    longitude=city['coordinates']['lon'],
+                ) for city in requests.get(URL_WITH_CITIES).json()
+                if city['country_code'] == 'RU'
+                and city.get('name') is not None
+            )
+        except (AttributeError, KeyError, TypeError):
+            raise ValueError(
+                'Неверный формат данных для загрузки таблицы городов'
+            )
