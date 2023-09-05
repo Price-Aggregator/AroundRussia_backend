4b5744c9968a439264dbd8b0ccddd73dd166360d
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-09-05 23:33:28 +0300

    Relocate FAQ constants

--- backend/faq/models.py
+++ backend/faq/models.py
@@ -3,19 +3,18 @@ from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-FAQ_CACHE_KEY = 'faq'
-MAX_SIZE = 500
+from api.constants import FAQ_CACHE_KEY, FAQ_LENGTH
 
 
 class FAQ(models.Model):
-    question = models.CharField(max_length=MAX_SIZE, unique=True)
+    question = models.CharField(max_length=FAQ_LENGTH, unique=True)
     answer = models.TextField()
 
     class Meta:
         ordering = ('question',)
 
     def __str__(self):
-        return f'{self.question}: {self.answer[:MAX_SIZE]}'
+        return f'{self.question}: {self.answer[:FAQ_LENGTH]}'
 
 
 @receiver([post_save, post_delete], sender=FAQ)
